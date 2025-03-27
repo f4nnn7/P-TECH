be0854272b290eed50b25905d19bf2322dd6e875
f4nnn7 <f4nnn76arc14@gmail.com>
2025-03-27 11:33:39 -0600

    Exercise 7
    Exercise 7

---
+++ RAMA_3/7.py
@@ -0,0 +1,12 @@
+def amigos():
+    mylist=['damian','javier','fili','alex','isaac']
+    for a in mylist:
+        print(a)
+    
+def padres():
+    mylist=['Maria Villasenor','Jose Garcia']
+    for a in mylist:
+        print(a)
+    
+padres()    
+amigos()
